fab36303097c75530a2fe76f9e507d18540b8012
Asia Alnahi <asia.alius@gmail.com>
2021-05-05 12:18:57 -0700

    Create Post Model

--- models/Post.js
+++ models/Post.js
@@ -13,7 +13,24 @@ Post.init({
     title: {
         type: DataTypes.STRING,
         allowNull: false,
-        
-
-    }
-})
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id',
+        },
+    },
+},
+{
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+}
+);
+module.exports = Post;
