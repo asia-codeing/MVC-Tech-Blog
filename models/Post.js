95bf3bb734fad4fce1a9eb7df030942ffd3bc817
Asia Alnahi <asia.alius@gmail.com>
2021-05-05 11:26:50 -0700

    Working on post Model

---
+++ models/Post.js
@@ -0,0 +1,19 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        
+
+    }
+})
