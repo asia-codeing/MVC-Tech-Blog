a4d5abfd74ad4a70014cfb0aa1c05de87fb49c4d
Asia Alnahi <asia.alius@gmail.com>
2021-05-05 15:25:35 -0700

    Associate the models in the index file

--- models/index.js
+++ models/index.js
@@ -4,21 +4,32 @@ const Comment = require('./Comment');
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
 });
 
-Post.belongsToMany(User, {
-    foreignKey: 'user_id'
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 User.hasMany(Comment, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE' 
+    onDelete: 'CASCADE'
 });
 
-Comment.belongsToMany(Post, {
-    foreignKey: 'post_id'
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
 module.exports = {
     User,
     Post,
