1f93e2fa5811d6ca5eb5bb300860f55fecb6d603
Asia Alnahi <asia.alius@gmail.com>
2021-05-20 14:26:10 -0700

    update the post handlebars

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,17 +3,14 @@ const { User } = require('../models');
 const userData = [
     {
       name: "Sal",
-      email: "sal@hotmail.com",
-     password: "password12345"
+      password: "password12345"
     },
     {
       name: "Lernantino",
-      email: "lernantino@gmail.com",
       password: "password12345"
     },
     {
       name: "Amiko",
-      email: "amiko2k20@aol.com",
       password: "password12345"
     }
 ]
