fcdc24100e49d94514dffcdb933cc87b134a4d97
Asia Alnahi <asia.alius@gmail.com>
2021-05-20 14:01:52 -0700

    update User Model

--- models/User.js
+++ models/User.js
@@ -19,14 +19,14 @@ User.init({
         type:DataTypes.STRING,
         allowNull: false,
     },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: true,
-        unique: true,
-        validate: {
-            isEmail:true,
-        }
-    },
+    // email: {
+    //     type: DataTypes.STRING,
+    //     allowNull: true,
+    //     unique: true,
+    //     validate: {
+    //         isEmail:true,
+    //     }
+    // },
     password: {
         type: DataTypes.STRING,
         allowNull: false,
